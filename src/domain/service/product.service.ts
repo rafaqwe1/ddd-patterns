d6a5bde464c3e9d1593cd5c324c50e389a62d89a
Rafael Rodrigues <rafaqwe1@gmail.com>
2023-02-28 20:08:05 -0300

    feat: implementing customer, order and product

---
+++ src/domain/service/product.service.ts
@@ -0,0 +1,7 @@
+import Product from "../entity/product";
+
+export default class ProductService {
+    static increasePrice(products: Product[], percentage: number): void {
+        products.forEach(product => product.changePrice(product.price * (1 + percentage/100)))
+    }
+}
